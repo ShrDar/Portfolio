28bdd5c6931267c5edd55d6790ce6fa2ee639ec0
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2023-01-25 11:11:54 +0545

    Added: Dark mode testing

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ hamburger=document.querySelector(".hamburger");
             navBar=document.querySelector(".nav-bar");
             navBar.classList.toggle("active");
         }
-        
+var icon= document.getElementById("icon");
+icon.onclick=function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains(dark-theme)){
+        b=document.getElementById("seci")
+        if(b.src.match("comp.png")){
+            b.src="none";
+        }
+    }
+}        
