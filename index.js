5f463ac22399bbcc1c52a011edc049d93f1a3dc8
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2023-01-25 19:20:59 +0545

    Added: Made dark mode responsive

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ hamburger=document.querySelector(".hamburger");
 var icon= document.getElementById("icon");
 icon.onclick=function(){
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains(dark-theme)){
-        b=document.getElementById("seci")
+    if(document.body.classList.contains("dark-theme")){
+        b=document.getElementById("seci");
         if(b.src.match("comp.png")){
-            b.src="none";
+            b.src="darkcomp.gif";
+            b.style.width="270px";
+            b.style.height="200px";
+            var mediaQueryCondition= window.matchMedia('(max-width: 900px)');
+            if(mediaQueryCondition.matches){
+                b.style.width="220px";
+                b.style.height="160px";
+            }
         }
+    }
+    else{
+        b=b=document.getElementById("seci");
+        b.src="comp.png";
+        b.style.width="240px";
+        b.style.height="210px";
+        var mediaQueryCondition= window.matchMedia('(max-width: 900px)');
+            if(mediaQueryCondition.matches){
+                b.style.width="180px";
+                b.style.height="150px";
+            }
     }
 }        
