5466573462085e86fea8cc560751d33363030d8e
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2023-02-09 13:04:18 +0545

    Added: Index page fixed and home css adjusted

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ function validation(){
     var username=document.getElementById("Email").value;
     var password=document.getElementById("Password").value;
     if(username==""){
-        alert("Username not entered");
+        alert("Username not entered(Username: Darshan Password: 12345)");
         return false;
     }
     else if(password==""){
-    alert("Password not entered");
+    alert("Password not entered(Username: Darshan Password: 12345)");
     return false;
     }
     else if(password.length<5){
-        alert("Min. length 5 words");
+        alert("Min. length 5 words(Username: Darshan Password: 12345)");
         return false;
     }
     else if(username=="Darshan" && password=="12345"){
         alert("Log in Successful");
     }
     else{
-        alert("Incorrect E-mail or Password")
+        alert("Incorrect E-mail or Password(Username: Darshan Password: 12345)")
         return false;
     }
 }
