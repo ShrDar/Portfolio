c37a24d8a331dc1aeac45015fb1b609f3768592b
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2023-06-09 19:59:57 +0545

    Added: Adjusted Project Electronics

--- Project-Electronics/home.js
+++ Project-Electronics/home.js
@@ -1,3 +1,45 @@
-var isScrolling = false;
+const applicances=[{
+    name: 'Fridge',
+    url: 'https://www.daraz.com.np/refrigerators/?city-electronics&from=wangpu',
+    img: 'Pictures/fridge.png'
+}, {
+    name: 'Washing Machine',
+    url: 'https://www.daraz.com.np/washing-machines/?city-electronics&from=wangpu',
+    img: 'Pictures/washing-machine.png'
+}, {
+    name: 'Air Conditioner',
+    url: 'https://www.daraz.com.np/air-conditioners/?city-electronics&from=wangpu',
+    img: 'Pictures/air-conditioner.png'
+}, {
+    name: 'Television',
+    url: 'https://www.daraz.com.np/smart-tvs/?city-electronics&from=wangpu',
+    img: 'Pictures/tv.png'
+}, {
+    name: 'Microwave',
+    url: 'https://www.daraz.com.np/microwaves/?city-electronics&from=wangpu',
+    img: 'Pictures/microwave.png'
+}, {
+    name: 'Vaccum',
+    url: 'https://www.daraz.com.np/vacuum-cleaners/?city-electronics&from=wangpu',
+    img: 'Pictures/vaccum.png'
+}];
 
+let homeHtml='';
+
+applicances.forEach((appliance) => {
+    let name = appliance.name;
+    let url = appliance.url;
+    let img = appliance.img;
+    
+    const html=`
+        <div class="items2">
+            <img class='electronic' src="${img}" alt="refrigerator">
+            <a href="${url}" target="_blank" rel="nofollow"><button>${name}</button></a>
+        </div>
+    `
+    homeHtml += html;
+
+    
+});
+document.querySelector('.js-items').innerHTML = homeHtml;
 
